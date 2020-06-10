39786b2e4cf28789ca82fbffd2678fc25f1f45d3
W. Nicholas Greene <wngreene_google@wng-k80-1>
2020-06-10 12:18:34 -0400

    Add training scripts.

---
+++ docker_train.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+docker run --runtime=nvidia --rm -it \
+       -v /home/wngreene_google/Projects/data:/data \
+       stereonet \
+       /bin/bash -c "/stereonet/train.sh"
---
+++ train.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+DATAPATH=/data/sceneflow
+
+./main8multi.py --datapath ${DATAPATH}
